57de5e0669c5f4036763ffd213357d7a800fd2ca
ΛΑΖΑΡΟΣ ΖΗΣΚΟΣ <ziskoslaz@gmail.com>
2025-01-25 17:59:37 +0200

    work on auth form

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,7 +1,8 @@
+import AuthForm from "@/components/forms/AuthForm";
 import React from "react";
 
 const SignIn = () => {
-  return <div>SignIn</div>;
+  return <AuthForm />;
 };
 
 export default SignIn;
